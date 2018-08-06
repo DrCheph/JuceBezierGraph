1b37cdb4d34112a0e9c629a48b6be76f04e8252b
Sam Ferguson <samferguson@Sams-MacBook-Pro-2.local>
2018-08-06 10:02:04 -0400

    Copyright info

--- Source/MainGraph.h
+++ Source/MainGraph.h
@@ -5,6 +5,21 @@
     Created: 29 Jul 2018 11:45:03am
     Author:  Sam Ferguson
 
+ This file is part of JuceBezierGraph.
+ 
+ Foobar is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Foobar is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ 
   ==============================================================================
 */
 
--- Source/NthGraph.h
+++ Source/NthGraph.h
@@ -9,6 +9,21 @@ This is a framework for a GUI element that alows the user to control a bunch of 
  
 written with the JUCE framework
  
+ This file is part of JuceBezierGraph.
+ 
+ Foobar is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Foobar is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ 
   ==============================================================================
 */
 
